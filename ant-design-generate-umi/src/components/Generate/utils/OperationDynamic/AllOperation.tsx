3ba807ce909709c1c15f1368597ec91b0a655ce9
zhaoqizhi <hearhear@foxmail.com>
2022-11-13 12:14:15 +0800

    111

---
+++ ant-design-generate-umi/src/components/Generate/utils/OperationDynamic/AllOperation.tsx
@@ -0,0 +1,32 @@
+import * as Icons from '@ant-design/icons';
+import PropTypes from 'prop-types';
+import IconsDynamic from '../../subComps/IconsDynamic';
+
+const AllOperation = (props: any) => {
+  const operation = props.operation;
+  const onEvent = props.onEvent;
+
+  let returnComponent = <></>;
+  if (!operation) {
+    console.error('operation不能为空。');
+    return returnComponent;
+  }
+  // console.debug('AllOperation', props);
+  switch (operation) {
+    case 'detail':
+      returnComponent = <IconsDynamic key="FileSearchOutlined" iconName="FileSearchOutlined" tooltip="详情" onEvent={onEvent} />;
+      break;
+    case 'edit':
+      returnComponent = <IconsDynamic key="EditOutlined" iconName="EditOutlined" tooltip="编辑" onEvent={onEvent} />;
+      break;
+    case 'delete':
+      returnComponent = <IconsDynamic key="DeleteOutlined" iconName="DeleteOutlined" tooltip="删除" onEvent={onEvent} />;
+      break;
+  }
+  return returnComponent;
+};
+AllOperation.propTypes = {
+  operation: PropTypes.object,
+  onEvent: PropTypes.func,
+};
+export default AllOperation;
---
+++ ant-design-generate-umi/src/components/Generate/utils/OperationDynamic/index.tsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import { Button, Popconfirm, Space, Upload } from 'antd';
+import AllOperation from './AllOperation';
+
+export const OperationDynamic = (operationArr: Array<string>, onEvent: (_: React.ReactNode, entity: any, index: number, type: string) => void, columnRender: any) => {
+  let returnComponent = <></>;
+  if (!operationArr) {
+    console.error('operationArr不能为空。');
+    return returnComponent;
+  }
+  const myOnEvent = (type: any) => {
+    onEvent(columnRender.reactNode, columnRender.entity, columnRender.index, type);
+  };
+
+  console.debug('OperationDynamic', operationArr, onEvent, columnRender);
+  return (
+    <>
+      <Space>
+        {operationArr?.map((operationItem: any, index: number) => {
+          return <AllOperation operation={operationItem} onEvent={() => myOnEvent(operationItem)} key={index} />;
+        })}
+      </Space>
+    </>
+  );
+};
